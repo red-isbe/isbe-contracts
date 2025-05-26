2e25cbd88997ded6c4f637e05c66b8e029c06af0
Alberto Molina <alberto@io.builders>
2025-05-26 13:27:19 +0200

    NatSpec doc added to AccessControl
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -4,11 +4,19 @@ pragma solidity ^0.8.28;
 import {IAccessControl} from './IAccessControl.sol';
 import {AccessControlInternal} from './AccessControlInternal.sol';
 
+/// @title AccessControl
+/// @notice Implements role-based access control mechanisms
+/// @dev Inherits from IAccessControl and AccessControlInternal, providing external role management functions
 contract AccessControl is IAccessControl, AccessControlInternal {
+    /// @notice Constructor that assigns the deployer as the default admin
     constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
+    /// @notice Grants a role to an account
+    /// @dev Caller must have the admin role for the given role
+    /// @param role The role identifier (bytes32 hash)
+    /// @param account The address to which the role is granted
     function grantRole(
         bytes32 role,
         address account
@@ -16,6 +24,10 @@ contract AccessControl is IAccessControl, AccessControlInternal {
         _grantRole(role, account);
     }
 
+    /// @notice Revokes a role from an account
+    /// @dev Caller must have the admin role for the given role
+    /// @param role The role identifier (bytes32 hash)
+    /// @param account The address from which the role is revoked
     function revokeRole(
         bytes32 role,
         address account
@@ -23,6 +35,10 @@ contract AccessControl is IAccessControl, AccessControlInternal {
         _revokeRole(role, account);
     }
 
+    /// @notice Changes the admin role of a given role
+    /// @dev Caller must have the current admin role for the role
+    /// @param role The role whose admin role is being changed
+    /// @param adminRole The new admin role to assign
     function setRoleAdmin(
         bytes32 role,
         bytes32 adminRole
@@ -30,10 +46,17 @@ contract AccessControl is IAccessControl, AccessControlInternal {
         _setRoleAdmin(role, adminRole);
     }
 
+    /// @notice Allows the caller to renounce a role they hold
+    /// @dev Can only be called by the account itself to renounce its own role
+    /// @param role The role to renounce
     function renounceRole(bytes32 role) external virtual override {
         _revokeRole(role, _msgSender());
     }
 
+    /// @notice Checks if an account has a specific role
+    /// @param role The role identifier (bytes32 hash)
+    /// @param account The address to check
+    /// @return True if the account has the role, false otherwise
     function hasRole(
         bytes32 role,
         address account
@@ -41,6 +64,9 @@ contract AccessControl is IAccessControl, AccessControlInternal {
         return _hasRole(role, account);
     }
 
+    /// @notice Returns the admin role associated with a specific role
+    /// @param role The role to query
+    /// @return The admin role that controls the given role
     function getRoleAdmin(
         bytes32 role
     ) external view virtual override returns (bytes32) {
