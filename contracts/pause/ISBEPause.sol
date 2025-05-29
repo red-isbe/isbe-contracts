89d1dd7c14c3905b6c929d54d7e26a0206ae4e52
Alberto Molina <alberto@io.builders>
2025-05-29 09:21:32 +0200

    virtual removed
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- contracts/pause/ISBEPause.sol
+++ contracts/pause/ISBEPause.sol
@@ -16,7 +16,7 @@ contract ISBEPause is Pause, AccessControl {
 
     uint256 private constant _ISBE_AUTHORIZATION_LEVEL = type(uint256).max;
 
-    function _checkPauserRoles() internal view virtual override {
+    function _checkPauserRoles() internal view override {
         bytes32[] memory roles = new bytes32[](2);
         roles[0] = _PAUSER_ROLE;
         roles[1] = _ISBE_ROLE;
@@ -26,7 +26,7 @@ contract ISBEPause is Pause, AccessControl {
 
     function _getAuthorityLevel(
         address _account
-    ) internal view virtual override returns (uint256) {
+    ) internal view override returns (uint256) {
         if (_hasRole(_PAUSER_ROLE, _account))
             return _PAUSER_AUTHORIZATION_LEVEL;
         if (_hasRole(_ISBE_ROLE, _account)) return _ISBE_AUTHORIZATION_LEVEL;
