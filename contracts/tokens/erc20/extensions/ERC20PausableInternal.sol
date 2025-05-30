fa2789f1842b524699bd5f697b30e45aa056fd37
Alberto Molina <alberto@io.builders>
2025-05-30 11:40:57 +0200

    pausable added to test wrapper
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- contracts/tokens/erc20/extensions/ERC20PausableInternal.sol
+++ contracts/tokens/erc20/extensions/ERC20PausableInternal.sol
@@ -5,14 +5,13 @@ import {ERC20} from '../ERC20.sol';
 
 /// @title ERC20Pausable
 /// @notice This abstract contract extends ERC20 functionality to support pausable token transfers.
-abstract contract ERC20Pausable is ERC20 {
+abstract contract ERC20PausableInternal is ERC20 {
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal virtual override {
-        super._beforeTokenTransfer(from, to, amount);
-
         _requireNotPaused();
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
