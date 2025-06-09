8d068c3e62128abe919fdf4a5179f3d19d4d7844
Alberto Molina <alberto@io.builders>
2025-06-09 16:04:43 +0200

    initialization updated
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- test/initialization.ts
+++ test/initialization.ts
@@ -1,115 +1,59 @@
 import { ethers } from 'hardhat'
-import { Signer } from 'ethers'
 import {
     ERC20TestWrapper,
-    EIP2535AccessControl__factory,
-    EIP2535AccessControl,
-    ERC20TestWrapper__factory,
-    DiamondCutAccessControlFacet__factory,
     DiamondCutAccessControlFacet,
-    DiamondLoupeFacet__factory,
     DiamondLoupeFacet,
-    AccessControlFacet__factory,
-    AccessControlFacet,
-    ISBEPauseFacet__factory,
-    ISBEPauseFacet,
     AccessControl,
     ISBEPause,
-    Ownable2StepFacet__factory,
-    OwnableFacet__factory,
-    AssetEventTrackerTestWrapper__factory,
-    HashTimestampTestWrapper__factory,
     AssetEventTrackerTestWrapper,
-    HashTimestamp,
-    Ownable2StepFacet,
     Ownable2Step,
-    OwnableFacet,
     Ownable,
-    MockTimestampFacet__factory,
-    MockTimestampFacet,
     MockTimestamp,
     HashTimestampTestWrapper,
 } from '../typechain-types'
 import { DEFAULT_ADMIN_ROLE } from './constants'
 
 export async function deployAll(isOwnable: boolean = false) {
-    let EIP2535AccessControlFactory: EIP2535AccessControl__factory
-    let DiamondCutAccessControlFacetFactory: DiamondCutAccessControlFacet__factory
-    let DiamondLoupeFacetFactory: DiamondLoupeFacet__factory
-    let AccessControlFacetFactory: AccessControlFacet__factory
-    let Ownable2StepFacetFactory: Ownable2StepFacet__factory
-    let OwnableFacetFactory: OwnableFacet__factory
-    let ISBEPauseFacetFactory: ISBEPauseFacet__factory
-    let ERC20TestWrapperFactory: ERC20TestWrapper__factory
-    let AssetEventTrackerTestWrapperFactory: AssetEventTrackerTestWrapper__factory
-    let HashTimestampTestWrapperFactory: HashTimestampTestWrapper__factory
-    let MockTimestampFacetFactory: MockTimestampFacet__factory
+    const [owner] = await ethers.getSigners()
 
-    let diamondCutFacet: DiamondCutAccessControlFacet
-    let diamondLoupeFacet: DiamondLoupeFacet
-    let accessControlFacet: AccessControlFacet
-    let ownable2StepFacet: Ownable2StepFacet
-    let ownableFacet: OwnableFacet
-    let pauseFacet: ISBEPauseFacet
-    let erc20Facet: ERC20TestWrapper
-    let assetEventTrackerFacet: AssetEventTrackerTestWrapper
-    let hashTimestampFacet: HashTimestamp
-    let mockTimestampFacet: MockTimestampFacet
-
-    let diamondProxy: EIP2535AccessControl
-
-    let facetAddresses: string[]
-
-    let erc20: ERC20TestWrapper
-    let pause: ISBEPause
-    let accessControl: AccessControl
-    let hashTimestamp: HashTimestamp
-    let assetEventTracker: AssetEventTrackerTestWrapper
-    let ownable2Step: Ownable2Step
-    let ownable: Ownable
-    let diamondCutAccessControl: DiamondCutAccessControlFacet
-    let diamondLoupe: DiamondLoupeFacet
-    let mockTimestamp: MockTimestamp
-
-    let owner: Signer
-    ;[owner] = await ethers.getSigners()
-
-    EIP2535AccessControlFactory = await ethers.getContractFactory(
+    const EIP2535AccessControlFactory = await ethers.getContractFactory(
         'EIP2535AccessControl'
     )
-    DiamondCutAccessControlFacetFactory = await ethers.getContractFactory(
+    const DiamondCutAccessControlFacetFactory = await ethers.getContractFactory(
         'DiamondCutAccessControlFacet'
     )
-    DiamondLoupeFacetFactory =
+    const DiamondLoupeFacetFactory =
         await ethers.getContractFactory('DiamondLoupeFacet')
 
-    AccessControlFacetFactory =
+    const AccessControlFacetFactory =
         await ethers.getContractFactory('AccessControlFacet')
-    Ownable2StepFacetFactory =
+    const Ownable2StepFacetFactory =
         await ethers.getContractFactory('Ownable2StepFacet')
-    OwnableFacetFactory = await ethers.getContractFactory('OwnableFacet')
-    ISBEPauseFacetFactory = await ethers.getContractFactory('ISBEPauseFacet')
-    ERC20TestWrapperFactory =
+    const OwnableFacetFactory = await ethers.getContractFactory('OwnableFacet')
+    const ISBEPauseFacetFactory =
+        await ethers.getContractFactory('ISBEPauseFacet')
+    const ERC20TestWrapperFactory =
         await ethers.getContractFactory('ERC20TestWrapper')
-    AssetEventTrackerTestWrapperFactory = await ethers.getContractFactory(
+    const AssetEventTrackerTestWrapperFactory = await ethers.getContractFactory(
         'AssetEventTrackerTestWrapper'
     )
-    HashTimestampTestWrapperFactory = await ethers.getContractFactory(
+    const HashTimestampTestWrapperFactory = await ethers.getContractFactory(
         'HashTimestampTestWrapper'
     )
-    MockTimestampFacetFactory =
+    const MockTimestampFacetFactory =
         await ethers.getContractFactory('MockTimestampFacet')
 
-    diamondCutFacet = await DiamondCutAccessControlFacetFactory.deploy()
-    diamondLoupeFacet = await DiamondLoupeFacetFactory.deploy()
-    accessControlFacet = await AccessControlFacetFactory.deploy()
-    ownable2StepFacet = await Ownable2StepFacetFactory.deploy()
-    ownableFacet = await OwnableFacetFactory.deploy()
-    pauseFacet = await ISBEPauseFacetFactory.deploy()
-    erc20Facet = await ERC20TestWrapperFactory.deploy()
-    assetEventTrackerFacet = await AssetEventTrackerTestWrapperFactory.deploy()
-    hashTimestampFacet = await HashTimestampTestWrapperFactory.deploy()
-    mockTimestampFacet = await MockTimestampFacetFactory.deploy()
+    const diamondCutFacet = await DiamondCutAccessControlFacetFactory.deploy()
+    const diamondLoupeFacet = await DiamondLoupeFacetFactory.deploy()
+    const accessControlFacet = await AccessControlFacetFactory.deploy()
+    const ownable2StepFacet = await Ownable2StepFacetFactory.deploy()
+    const ownableFacet = await OwnableFacetFactory.deploy()
+    const pauseFacet = await ISBEPauseFacetFactory.deploy()
+    const erc20Facet = await ERC20TestWrapperFactory.deploy()
+    const assetEventTrackerFacet =
+        await AssetEventTrackerTestWrapperFactory.deploy()
+    const hashTimestampFacet = await HashTimestampTestWrapperFactory.deploy()
+    const mockTimestampFacet = await MockTimestampFacetFactory.deploy()
 
     await diamondCutFacet.waitForDeployment()
     await diamondLoupeFacet.waitForDeployment()
@@ -122,7 +66,7 @@ export async function deployAll(isOwnable: boolean = false) {
     await hashTimestampFacet.waitForDeployment()
     await mockTimestampFacet.waitForDeployment()
 
-    facetAddresses = [
+    const facetAddresses = [
         await diamondCutFacet.getAddress(),
         await diamondLoupeFacet.getAddress(),
         await accessControlFacet.getAddress(),
@@ -136,55 +80,58 @@ export async function deployAll(isOwnable: boolean = false) {
         await mockTimestampFacet.getAddress(),
     ]
 
-    diamondProxy = await EIP2535AccessControlFactory.deploy(facetAddresses, {
-        rbacs: [
-            {
-                role: DEFAULT_ADMIN_ROLE,
-                members: [owner],
-            },
-        ],
-        init: ethers.ZeroAddress,
-        initCalldata: '0x',
-    })
+    const diamondProxy = await EIP2535AccessControlFactory.deploy(
+        facetAddresses,
+        {
+            rbacs: [
+                {
+                    role: DEFAULT_ADMIN_ROLE,
+                    members: [owner],
+                },
+            ],
+            init: ethers.ZeroAddress,
+            initCalldata: '0x',
+        }
+    )
     await diamondProxy.waitForDeployment()
 
-    erc20 = ERC20TestWrapperFactory.attach(
+    const erc20 = ERC20TestWrapperFactory.attach(
         await diamondProxy.getAddress()
     ) as ERC20TestWrapper
 
-    pause = ISBEPauseFacetFactory.attach(
+    const pause = ISBEPauseFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as ISBEPause
 
-    accessControl = AccessControlFacetFactory.attach(
+    const accessControl = AccessControlFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as AccessControl
 
-    ownable2Step = Ownable2StepFacetFactory.attach(
+    const ownable2Step = Ownable2StepFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as Ownable2Step
 
-    ownable = OwnableFacetFactory.attach(
+    const ownable = OwnableFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as Ownable
 
-    assetEventTracker = AssetEventTrackerTestWrapperFactory.attach(
+    const assetEventTracker = AssetEventTrackerTestWrapperFactory.attach(
         await diamondProxy.getAddress()
     ) as AssetEventTrackerTestWrapper
 
-    hashTimestamp = HashTimestampTestWrapperFactory.attach(
+    const hashTimestamp = HashTimestampTestWrapperFactory.attach(
         await diamondProxy.getAddress()
     ) as HashTimestampTestWrapper
 
-    diamondCutAccessControl = DiamondCutAccessControlFacetFactory.attach(
+    const diamondCutAccessControl = DiamondCutAccessControlFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as DiamondCutAccessControlFacet
 
-    diamondLoupe = DiamondLoupeFacetFactory.attach(
+    const diamondLoupe = DiamondLoupeFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as DiamondLoupeFacet
 
-    mockTimestamp = MockTimestampFacetFactory.attach(
+    const mockTimestamp = MockTimestampFacetFactory.attach(
         await diamondProxy.getAddress()
     ) as MockTimestamp
 
