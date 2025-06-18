128e370b32bcc349a9be8077398ab901a9dc8e51
Marcos Serradilla Diez <marcos@io.builders>
2025-06-18 09:26:02 +0200

    Update `.solcover.js` to expand skipFiles list (#95)
    * Update `.solcover.js` to expand skipFiles list
    
    Added multiple test wrapper files to the skipFiles configuration in the `.solcover.js` file to streamline coverage reporting by excluding internal test-only contracts.
    
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>
    
    * Update `test:coverage` script in `package.json`
    
    Added `hardhat clean` step and `.solcover.js` configuration to ensure accurate and consistent test coverage reporting.
    
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>
    
    * Update `package.json` pre-commit and test:coverage scripts
    
    Refined `test:coverage` script to use a relative path for `.solcover.js`. Enhanced `pre-commit` script by adding `clean:cache` step for improved pre-commit checks.
    
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>
    
    * solcover bug fixed
    
    Signed-off-by: Alberto Molina <alberto@io.builders>
    
    ---------
    
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>
    Signed-off-by: Alberto Molina <alberto@io.builders>
    Co-authored-by: Alberto Molina <alberto@io.builders>

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,16 @@
 module.exports = {
-    skipFiles: ['testwrapper/reentrancyguard/ReentrancyGuardTestWrapper.sol'],
+    skipFiles: [
+        'testwrapper/assetevent/AssetEventTrackerTestWrapper.sol',
+        'testwrapper/factory/facet/CounterFacetInternal.sol',
+        'testwrapper/factory/facet/CounterFacetTestWrapper.sol',
+        'testwrapper/factory/facet/CounterV2FacetTestWrapper.sol',
+        'testwrapper/hashtimestamp/HashTimestampTestWrapper.sol',
+        'testwrapper/mockTimestamp/IMockTimestamp.sol',
+        'testwrapper/mockTimestamp/MockTimestamp.sol',
+        'testwrapper/mockTimestamp/MockTimestampFacet.sol',
+        'testwrapper/reentrancyguard/ReentrancyGuardTestWrapper.sol',
+        'testwrapper/tokens/erc20/ERC20TestWrapper.sol',
+        'testwrapper/tokens/erc20/ERC20TestWrapperTransparent.sol',
+        'testwrapper/tokens/erc20/ERC20TestWrapperUUPS.sol',
+    ],
 }
