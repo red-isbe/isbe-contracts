0a9863371989c30a66a74f884175e24763d7b00c
Fernando Lopez <cainuriel@gmail.com>
2025-07-14 13:04:25 +0200

    Split ERC3643 IddentityStorageManagement (#132) in to: - IIdentityStorageManagement: Interface to manage storage identities - IIdentityStorageRegistryManagement: Interface to manage linked registries.

--- contracts/tokens/erc3643/identityregistrystorage/IIdentityRegistryStorage.sol
+++ contracts/tokens/erc3643/identityregistrystorage/IIdentityRegistryStorage.sol
@@ -1,5 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+import {
+    IIdentityStorageRegistryManagement
+} from './modules/IIdentityStorageRegistryManagement.sol';
+import {
+    IIdentityStorageManagement
+} from './modules/IIdentityStorageManagement.sol';
+
+/**
+ * @title IIdentityRegistryStorage
+ * @notice Interface to manage storage identitys and their linked registries
+ * @dev Combines identity lifecycle operationss and registry binding logic
+ * @author ISBE
+ */
 // solhint-disable-next-line no-empty-blocks
-interface IIdentityRegistryStorage {}
+interface IIdentityRegistryStorage is
+    IIdentityStorageRegistryManagement,
+    IIdentityStorageManagement
+{}
