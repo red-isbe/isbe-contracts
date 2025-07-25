824690445ec870ae84f45ad18eb03bc6efaca484
Marcos Serradilla Diez <marcos@io.builders>
2025-07-25 14:03:59 +0200

    Refactor import statements for improved readability and alignment with project formatting standards across multiple contracts.
    Signed-off-by: Marcos Serradilla Diez <marcos@io.builders>

--- contracts/tokens/erc3643/identityregistry/IIdentityRegistry.sol
+++ contracts/tokens/erc3643/identityregistry/IIdentityRegistry.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {
-    IIdentityManagement
-} from './modules/identitymanagement/IIdentityManagement.sol';
-import {
-    IRegistriesManagement
-} from './modules/identitymanagement/IRegistriesManagement.sol';
+import {IIdentityManagement} from './modules/identitymanagement/IIdentityManagement.sol';
+import {IRegistriesManagement} from './modules/identitymanagement/IRegistriesManagement.sol';
 import {IVerification} from './modules/identitymanagement/IVerification.sol';
 
 /**
